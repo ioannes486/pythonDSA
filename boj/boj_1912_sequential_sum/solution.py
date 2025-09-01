8393649f7647c09870a48640e080465336f6e30d
박지원 <love990890@naver.com>
2025-09-01 17:23:58 +0900

    연속합

---
+++ boj/boj_1912_sequential_sum/solution.py
@@ -0,0 +1,27 @@
+"""TODO:
+- 상수
+
+- 변수
+    N ; 수열의 길이
+- 조건(제약사항)
+    수는 한개 이상 선택해야 함
+- 구하는 값
+    연속된 몇개의 수를 선택해서 구할 수 있는 합 중 가장 큰 값
+- 아이디어
+"""
+
+def recur(cursum):
+
+    # 1. 증가하는 수열 더하기
+    N = 3
+
+    for i in range(N):
+
+
+    return
+
+# 입력
+N = int(input)
+arr = list(map(int, input().split()))
+# 출력
+print(solve())
